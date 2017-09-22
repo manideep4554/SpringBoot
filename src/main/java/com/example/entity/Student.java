942c56ed07da3811a244b6bdbb4f5cc88f476807
manideep <manideep4554@gmail.com>
2017-09-22 11:32:31 -0500

    First Commit

---
+++ src/main/java/com/example/entity/Student.java
@@ -0,0 +1,18 @@
+package com.example.entity;
+
+public class Student {
+	
+	public Student(int id, String name, String course) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.course = course;
+	}
+
+	private int id;
+	private String name;
+	private String course;
+	
+	public Student() {}
+
+}
